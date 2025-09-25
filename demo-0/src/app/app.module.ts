1f2dd86130578a04ada0b87e43a96a0698bdaade
jannewitjes <45061820+jannewitjes@users.noreply.github.com>
2025-09-25 13:22:58 +0200

    Intro + First Look + Developer Tools

--- demo-0/src/app/app.module.ts
+++ demo-0/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { AppComponent } from './app.component';
 import { InMemoryDataService } from './in-memory-data.service';
 import { HomeComponent } from './home/home.component';
 
+import {Store, StoreModule} from "@ngrx/store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {environment} from "../environments/environment";
+
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
@@ -15,6 +19,12 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+      name: 'NgRx Demo App',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ demo-0/src/app/products/state/products.reducer.ts
@@ -0,0 +1,18 @@
+import {createAction, createReducer, on} from "@ngrx/store";
+
+export interface ProductsState {
+  showProductCode: boolean;
+}
+
+const initialState: ProductsState = {
+  showProductCode: false,
+};
+
+export const productsReducer = createReducer(
+  initialState,
+  on(
+    createAction('[Products Page] Toggle Show Product Code'), (state) => ({
+      ...state,
+      showProductCode: !state.showProductCode
+    }))
+  )

5c35d59f16579966788d9d1c487fd1262c408bdf
jannewitjes <45061820+jannewitjes@users.noreply.github.com>
2025-09-26 15:42:09 +0200

    Strongly typing actions

--- demo-0/src/app/products/state/products.reducer.ts
+++ demo-0/src/app/products/state/products.reducer.ts
@@ -1,18 +1,32 @@
 import {createAction, createReducer, on} from "@ngrx/store";
+import {ProductsApiActions, ProductsPageActions} from "./products.actions";
+import {Product} from "../product.model";
 
 export interface ProductsState {
   showProductCode: boolean;
+  loading: boolean;
+  products: Product[];
 }
 
 const initialState: ProductsState = {
   showProductCode: false,
+  loading: false,
+  products: []
 };
 
 export const productsReducer = createReducer(
   initialState,
-  on(
-    createAction('[Products Page] Toggle Show Product Code'), (state) => ({
+  on(ProductsPageActions.toggleShowProductCode, (state) => ({
       ...state,
       showProductCode: !state.showProductCode
-    }))
+    })),
+  on(ProductsPageActions.loadProducts, (state) => ({
+    ...state,
+    loading: true
+  })),
+  on(ProductsApiActions.productsLoadedSuccess, (state, {products}) => ({
+    ...state,
+    loading: false,
+    products
+  }))
   )

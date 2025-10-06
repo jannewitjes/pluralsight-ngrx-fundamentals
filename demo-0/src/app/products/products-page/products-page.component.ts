7361b296886495331ba0cd5e1a355b71603fcabd
jannewitjes <45061820+jannewitjes@users.noreply.github.com>
2025-10-06 11:49:10 +0200

    Component Store

--- demo-0/src/app/products/products-page/products-page.component.ts
+++ demo-0/src/app/products/products-page/products-page.component.ts
@@ -8,24 +8,30 @@ import {
   selectProductsShowProductCode,
   selectProductsTotal
 } from "../state/products.selector";
+import {ProductsStore} from "../products.store";
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  providers: [ProductsStore]
 })
 export class ProductsPageComponent {
-  products$ = this.store.select(selectProducts);
+  products$ = this.productsStore.products$;
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading)
   showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage$ = this.store.select(selectProductsErrorMessage);
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private productsStore: ProductsStore) {
     this.store.subscribe(state => console.log(state));
   }
 
   toggleShowProductCode() {
     this.store.dispatch(ProductsPageActions.toggleShowProductCode());
+  }
+
+  ngOnInit() {
+    this.productsStore.getProducts();
   }
 }
---
+++ demo-0/src/app/products/products.store.ts
@@ -0,0 +1,28 @@
+import {Product} from "./product.model";
+import {Injectable} from "@angular/core";
+import {ProductsService} from "./products.service";
+import {ComponentStore} from "@ngrx/component-store";
+import {exhaustMap, tap} from "rxjs";
+
+interface ProductsState {
+  products: Product[]
+}
+
+@Injectable()
+export class ProductsStore extends ComponentStore<ProductsState> {
+  products$ = this.select((state) => state.products)
+
+  constructor (private productsService: ProductsService) {
+    super({products: []});
+  }
+
+  addProducts = this.updater((state, products: Product[]) => ({
+    ...state,
+    products
+  }));
+
+  getProducts = this.effect((trigger$) =>
+  trigger$.pipe(
+    exhaustMap(() => this.productsService.getAll().pipe(tap({ next: this.addProducts })))
+  ))
+}
